56369b47b47395eaedd3bb8459f05626c75377e3
BunjanMark <bunjan.mark476@gmail.com>
2023-07-27 23:35:43 +0800

    Navbar complete

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./header.css";
+const Header = () => {
+	return (
+		<div className="container">
+			<div className="header">Header</div>
+		</div>
+	);
+};
+
+export default Header;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import logo from "../../assets/logo.png";
+
+import "./navbar.css";
+const Navbar = () => {
+	const [clicked, setClick] = useState(false);
+	const handleClick = () => {
+		setClick(!clicked);
+	};
+	return (
+		<div className="nav">
+			<div className="container" id="nav">
+				<ul id="logo-section">
+					<img
+						src={logo}
+						alt="page logo"
+						className="logo"
+					/>
+					<h2>Sho.</h2>
+				</ul>
+				<ul
+					id="menu"
+					className={
+						clicked
+							? "menu active "
+							: "menu "
+					}
+				>
+					<li>
+						<a href="index.html">home</a>
+					</li>
+					<li>
+						<a href="index.html">work</a>
+					</li>
+					<li>
+						<a href="index.html">contact</a>
+					</li>
+				</ul>
+				<ul
+					id="mobile"
+					onClick={handleClick}
+					className="icon"
+				>
+					<span>
+						{clicked ? (
+							<FaTimes />
+						) : (
+							<FaBars />
+						)}
+					</span>
+				</ul>
+			</div>
+		</div>
+	);
+};
+
+export default Navbar;
